99f44a9e8f7dd53b72d1a3016c36b762ecfea168
chendo <chendoec@live.com>
2023-06-02 22:59:54 -0500

    Fixed Refresh Page SPA

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from '@context/GlobalProvider';
 import { GlobalStyle } from '@styles/GlobalStyle';
 
